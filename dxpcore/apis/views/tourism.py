9302022f6be8ed42bb7a44dfea3a40da2f2f647b
princek <princesamuelpks@gmail>
2025-03-15 10:00:17 +0000

    implement deletion of sites

--- dxpcore/apis/views/tourism.py
+++ dxpcore/apis/views/tourism.py
@@ -11,13 +11,13 @@ class TouristSiteListAPI(APIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request, *args, **kwargs):
-        '''Get all sites'''
+        '''Get all sites. Everyone can view the sites'''
         sites = TouristSite.objects.all().order_by('-created_at', 'name')
         serializer = TouristSiteSerialiser(sites, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        '''Create a new TouristSite'''
+        '''Create a new tourist site. Only staff can create a site'''
         user = request.user
         if not user.is_staff:
             return Response({'message': 'You are not authorized to create a tourist site'}, status=status.HTTP_401_UNAUTHORIZED)
@@ -34,3 +34,15 @@ class TouristSiteListAPI(APIView):
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, *args, **kwargs):
+        '''Delete a tourist site. Only staff can delete a site'''
+        user = request.user
+        if not user.is_staff:
+            return Response({'message': 'You are not authorized to delete a tourist site'}, status=status.HTTP_401_UNAUTHORIZED)
+        site_id = request.data.get('id')
+        site = TouristSite.objects.filter(id=site_id).first()
+        if site:
+            site.delete()
+            return Response({'message': 'Tourist site deleted successfully'}, status=status.HTTP_200_OK)
+        return Response({'message': 'Tourist site not found'}, status=status.HTTP_404_NOT_FOUND)
