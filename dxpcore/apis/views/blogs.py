55687278cd4908a76358ba1bfc92a08065038f6b
princek <princesamuelpks@gmail>
2025-04-25 22:00:42 +0000

    Create blogs.py

---
+++ dxpcore/apis/views/blogs.py
@@ -0,0 +1,55 @@
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apis.models import Blog, Hotel
+from apis.serializers import BlogSerializer, HotelSerializer
+
+
+class BlogsListAPI(APIView):
+    '''Blog List API endpoint'''
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, *args, **kwargs):
+        '''Get all/random blogs. Everyone can view the blogs'''
+        user = request.user
+        if user.is_staff or user.is_superuser:
+            blogs = Blog.objects.all().order_by('-created_at')
+        else:
+            _ = Blog.objects.filter(is_published=True).order_by('-created_at')
+            blogs = _.filter('?')[20]
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        '''Create a new blog. Only staff can create a blog'''
+        user = request.user
+        if not user.is_staff or not user.is_superuser:
+            return Response({'message': 'You are not authorized to create a hotel'}, status=status.HTTP_401_UNAUTHORIZED)
+        req_data = request.data.copy()
+        req_data['writer'] = user.id
+        serializer = BlogSerializer(data=req_data)
+        blog_id = request.data.get('id')
+        blog = None
+        if blog_id:
+            blog = Blog.objects.filter(id=blog_id).first()
+            if blog:
+                serializer = BlogSerializer(blog, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            if blog:
+                return Response({"message": "Blog updated successfully", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response({"message": "Blog Created Successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, *args, **kwargs):
+        '''Delete a blog. Only staff can delete a blog'''
+        user = request.user
+        if not user.is_staff:
+            return Response({'message': 'You are not authorized to delete a hotel'}, status=status.HTTP_401_UNAUTHORIZED)
+        blog_id = request.data.get('blog')
+        blog = Blog.objects.filter(id=blog_id).first()
+        if blog:
+            blog.delete()
+            return Response({'message': 'Blog deleted successfully'}, status=status.HTTP_200_OK)
+        return Response({'message': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
