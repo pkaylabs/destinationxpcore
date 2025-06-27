b9671fa0effe170615d1feb483d7ba098785c70c
princek <princesamuelpks@gmail>
2025-06-27 15:22:40 +0000

    remove restrictions on categories

--- dxpcore/apis/models.py
+++ dxpcore/apis/models.py
@@ -26,12 +26,6 @@ class Message(models.Model):
 
 class Hotel(models.Model):
     '''Model to store information about hotels'''
-    categories = (
-        (HotelCategory.STANDARD.name, HotelCategory.STANDARD.value),
-        (HotelCategory.SHORTSTAY.name, HotelCategory.SHORTSTAY.value),
-        (HotelCategory.FAMILY.name, HotelCategory.FAMILY.value),
-        (HotelCategory.BOOTIQUE.name, HotelCategory.BOOTIQUE.value),
-    )
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=500)
     phone = models.CharField(max_length=12)
@@ -51,19 +45,13 @@ class Hotel(models.Model):
     
 class Political(models.Model):
     '''Model to store information about political sites'''
-    categories = (
-        (PoliticalCategory.MINISTRY.name, PoliticalCategory.MINISTRY.value),
-        (PoliticalCategory.DEPARTMENT.name, PoliticalCategory.DEPARTMENT.value),
-        (PoliticalCategory.AGENCY.name, PoliticalCategory.AGENCY.value),
-        (PoliticalCategory.OTHERS.name, PoliticalCategory.OTHERS.value),
-    )
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=500)
     phone = models.CharField(max_length=12)
     email = models.EmailField(max_length=50)
     landmark = models.CharField(max_length=255)
     custodian = models.CharField(max_length=200)
-    category = models.CharField(max_length=50, choices=categories)
+    category = models.CharField(max_length=50,default=PoliticalCategory.OTHERS.value)
     description = models.TextField()
     image = models.ImageField(upload_to='political/', null=True, blank=True)
     second_image = models.ImageField(upload_to='political/', null=True, blank=True)
@@ -77,14 +65,6 @@ class Political(models.Model):
     
 class TouristSite(models.Model):
     '''Model to store information about tourist sites'''
-    categories = (
-        (TourismCategory.HISTORICAL.name, TourismCategory.HISTORICAL.value),
-        (TourismCategory.CULTURAL.name, TourismCategory.CULTURAL.value),
-        (TourismCategory.LEISURE.name, TourismCategory.LEISURE.value),
-        (TourismCategory.NATURE.name, TourismCategory.NATURE.value),
-        (TourismCategory.ENTERTAINMENT.name, TourismCategory.ENTERTAINMENT.value),
-        (TourismCategory.OTHERS.name, TourismCategory.OTHERS.value),
-    )
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=500)
     phone = models.CharField(max_length=12)
@@ -95,7 +75,7 @@ class TouristSite(models.Model):
     image = models.ImageField(upload_to='tourism/', null=True, blank=True)
     second_image = models.ImageField(upload_to='tourism/', null=True, blank=True)
     third_image = models.ImageField(upload_to='tourism/', null=True, blank=True)
-    category = models.CharField(max_length=50, choices=categories)
+    category = models.CharField(max_length=50, default=TourismCategory.OTHERS.value)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
