34191eaa47cde19cdf2e479d3cd07251ff0673e3
princek <princesamuelpks@gmail>
2025-09-13 22:23:02 +0000

    update

--- dxpcore/apis/views/accounts.py
+++ dxpcore/apis/views/accounts.py
@@ -471,7 +471,7 @@ class BlockUserAPIView(APIView):
         # check if user belongs to that chatroom
         if not chatroom_id:
             return Response({'error': 'Chatroom ID is required'}, status=status.HTTP_400_BAD_REQUEST)
-        chatroom = ChatRoom.objects.filter(room_id=chatroom_id).first()
+        chatroom = ChatRoom.objects.filter(name=chatroom_id).first()
         print(f"Room ID: {chatroom_id} | Chatroom: {chatroom}")
         if not chatroom:
             return Response({'error': 'Chatroom not found'}, status=status.HTTP_404_NOT_FOUND)
